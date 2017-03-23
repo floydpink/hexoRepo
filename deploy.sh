1c58d8a9909d5dbdc8990e0f976bcb33f9488680
Floyd Pink <imbleedingme@googlemail.com>
2017-03-23 16:50:04 -0400

    update Website and the HexoRepo

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
-hexo generate
-hexo d
-git add .
-git commit -m "update Website and the HexoRepo"
+hexo clean && hexo generate && hexo deploy
+git add . && git commit -m "update Website and the HexoRepo"
 git push origin master
